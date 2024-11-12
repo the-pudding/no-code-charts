e9deab8e64ed721b6a3875d431594f3c822951a4
Jan Diehm <jadiehm@gmail.com>
2024-11-12 14:54:30 -0600

    Initial commit

---
+++ svelte.config.js
@@ -0,0 +1,23 @@
+import adapterStatic from "@sveltejs/adapter-static";
+import { sveltePreprocess } from "svelte-preprocess";
+import autoprefixer from "autoprefixer";
+
+const preprocess = sveltePreprocess({
+	postcss: {
+		plugins: [autoprefixer]
+	}
+});
+
+// TODO switch
+
+const config = {
+	compilerOptions: {
+		runes: true
+	},
+	preprocess,
+	kit: {
+		adapter: adapterStatic({ strict: false })
+	}
+};
+
+export default config;
